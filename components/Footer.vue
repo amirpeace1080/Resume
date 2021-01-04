33ce44418dee53c174dbd33c279923d6f7ebcbc7
amirpeace1080 <amirpeace1080@gmail.com>
2021-01-04 11:33:50 +0330

    first commit

---
+++ components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <footer class="footer">
+      <div class="container text-center">
+        <span class="footer-text"
+          >Copyright ©saurabhkanswal 2019 | All Rights Reserved</span
+        >
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/Project.vue
@@ -0,0 +1,180 @@
+<template>
+  <div>
+    <div id="projects">
+      <div class="container">
+        <h1 class="text-center aboutHeading">پروژه ها</h1>
+        <div class="row pt-5 m-auto" id="projectCards">
+
+          <div class="col-md-6 col-lg-4 pb-3">
+            <div class="card card-custom bg-white border-white border-0">
+              <!--Enter Project Logo/Favicon here-->
+              <div class="card-custom-img" id="project1"></div>
+              <a href="#" target="_blank" class="grow text-right">
+                <div class="card-custom-avatar">
+                  <img
+                    class="img-fluid"
+                    src="http://rango.gigfa.com/_nuxt/img/5555764.jpg"
+                    alt="Avatar"
+                  />
+                </div>
+              </a>
+              <div class="card-body" style="overflow-y: auto">
+                <h4 class="card-title text-right">همه چی آموز</h4>
+                <p class="card-text text-right">
+                  ساخته شده با ناکست جی اس و ویوتیفای ،تقریبا تازه شروع کرده بودم حدودا دو ماه ونیم طول کشید که ساختم
+                </p>
+              </div>
+              <div
+                class="card-footer"
+                style="background: inherit; border-color: inherit"
+              >
+                <a
+                  href="http://rango.gigfa.com"
+                  class="btn btn-info"
+                  target="_blank"
+                  >بازدید</a
+                >
+                <a href="https://github.com/amirpeace1080/nuxt-hame-chi-amoz" class="btn btn-outline-primary">سورس کد</a>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 pb-3">
+            <div class="card card-custom bg-white border-white border-0">
+              <!--Enter Project Logo/Favicon here-->
+              <div class="card-custom-img" id="project2"></div>
+              <a href="#" target="_blank" class="grow">
+                <div class="card-custom-avatar">
+                  <img
+                    class="img-fluid"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSyWUAV2yn-kS8mvA1giT5ROJb0pE6-m-GNcfQIaqpJR3Va6JQk&usqp=CAU"
+                    alt="Avatar"
+                  />
+                </div>
+              </a>
+              <div class="card-body" style="overflow-y: auto">
+                <h4 class="card-title text-right">ال آی موزیک</h4>
+                <p class="card-text text-right">
+                  ساخته شده با ناکست جی اس و بوت استرپ ویو ، ای پی ای هم با پست من هست
+                </p>
+              </div>
+              <div
+                class="card-footer"
+                style="background: inherit; border-color: inherit"
+              >
+                <a href="http://limusic.gigfa.com" class="btn btn-info">بازدید</a>
+                <a href="https://github.com/amirpeace1080/limusic" type="button" class="btn btn-outline-primary">
+                  سورس کد
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 pb-3">
+            <div class="card card-custom bg-white border-white border-0">
+              <!--Enter Project Logo/Favicon here-->
+              <div class="card-custom-img" id="portfolio"></div>
+              <a href="#">
+                <div class="card-custom-avatar">
+                  <img
+                    class="img-fluid"
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                    alt="Avatar"
+                  />
+                </div>
+              </a>
+              <div class="card-body" style="overflow-y: auto">
+                <h4 class="card-title text-right">لرن اور فلو</h4>
+                <p class="card-text text-right">
+                  ساخته شده با ناکست جی اس و بوت استرپ ویو، ای پی ای رو تو نت پیدا کردم با کاتلین هست
+                </p>
+              </div>
+              <div
+                class="card-footer"
+                style="background: inherit; border-color: inherit"
+              >
+                <a href="http://learnoverflow.gigfa.com" class="btn btn-info">بازدید</a>
+                <a href="https://gitlab.com/amirpeace1080/stack" type="button" class="btn btn-outline-primary">
+                  سورس کد
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 pb-3">
+            <div class="card card-custom bg-white border-white border-0">
+              <!--Enter Project Logo/Favicon here-->
+              <div class="card-custom-img" id="project2"></div>
+              <a href="#" target="_blank" class="grow">
+                <div class="card-custom-avatar">
+                  <img
+                    class="img-fluid"
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                    alt="Avatar"
+                  />
+                </div>
+              </a>
+              <div class="card-body" style="overflow-y: auto">
+                <h4 class="card-title text-right">فارسی کردن پنل</h4>
+                <p class="card-text text-right">
+                  فارسی کردن پنل ادمین ،ساخته شده با ویو جی اس و ویوتیفای
+                </p>
+              </div>
+              <div
+                class="card-footer"
+                style="background: inherit; border-color: inherit"
+              >
+                <a target="_blank" href="http://paneladmin.gigfa.com/?i=1#/" class="btn btn-info">بازدید</a>
+                <a href="https://gitlab.com/amirpeace1080/paneladmin" type="button" class="btn btn-outline-primary">
+                  سورس کد
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+          <div class="col-md-6 col-lg-4 pb-3">
+            <div class="card card-custom bg-white border-white border-0">
+              <!--Enter Project Logo/Favicon here-->
+              <div class="card-custom-img" id="portfolio"></div>
+              <a href="#">
+                <div class="card-custom-avatar">
+                  <img
+                    class="img-fluid"
+                    src="https://res.cloudinary.com/dcl67luxn/image/upload/v1543875011/2.png"
+                    alt="Avatar"
+                  />
+                </div>
+              </a>
+              <div class="card-body" style="overflow-y: auto">
+                <h4 class="card-title">تک صفحه ای کردن قالب</h4>
+                <p class="card-text">
+                  تک صفحه ای کردن یه قالب آماده ، اولین پروژه ای هست که با ویو انجام دادم
+                </p>
+              </div>
+              <div
+                class="card-footer"
+                style="background: inherit; border-color: inherit"
+              >
+                <a target="_blank" href="http://shop10.gigfa.com" class="btn btn-info">بازدید</a>
+                <button type="button" class="btn btn-outline-primary" disabled>
+                  سورس کد
+                </button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
